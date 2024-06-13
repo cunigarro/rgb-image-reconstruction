c65f6343df5447a54e235fd925af32b12a779612
Christian Unigarro <christian.unigarro@zinobe.com>
2024-06-13 15:55:07 -0500

    Show result for the model.

--- app.py
+++ app.py
@@ -44,10 +44,16 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 # Entrenamiento del modelo
-for epoch in range(10):
+for epoch in range(3):
+    running_loss = 0.0
+    # Iterate over training batches
     for images, labels in dataloader_train:
         optimizer.zero_grad()
         outputs = net(images)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
+        running_loss += loss.item()
+
+    epoch_loss = running_loss / len(dataloader_train)
+    print(f"Epoch {epoch+1}, Loss: {epoch_loss:.10f}")
