4c70eb1cd04cc19512cc94621456b4d353686379
Christian Unigarro <christian.unigarro@zinobe.com>
2024-06-13 12:27:05 -0500

    Requirements defined and tester tool configuration.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from torch import nn
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
 
 class Net(nn.Module):
     def __init__(self, num_classes):
@@ -18,3 +21,25 @@ class Net(nn.Module):
         x = self.feature_extractor(x)
         x = self.classifier(x)
         return x
+
+# Cargar el dataset y crear el dataloader
+dataset_train = ImageFolder('datasets', transform=ToTensor())
+
+dataloader_train = DataLoader(
+    dataset_train, shuffle=True, batch_size=16
+)
+
+# Inicializar la red, la función de pérdida y el optimizador
+net = Net(num_classes=7)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+# Entrenamiento del modelo
+for epoch in range(10):
+    for images, labels in dataloader_train:
+        optimizer.zero_grad()
+        outputs = net(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
