f9e97ec9f073e7462715f7ea47ca86c343c6f8b9
Christian Unigarro <christian.unigarro@zinobe.com>
2024-06-13 11:50:56 -0500

    First convolutional.

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from torch.nn import Sequential
+from torch import nn
 
+class Net(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ELU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ELU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Flatten(),
+        )
+        self.classifier = nn.Linear(64*16*16, num_classes)
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = self.classifier(x)
+        return x
