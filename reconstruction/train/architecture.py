79809a8592045ab0c047135b17afcdb34c73fd16
Christian Unigarro <christian.unigarro@zinobe.com>
2024-07-13 21:57:28 -0500

    Improve arguments for activation functions.

--- reconstruction/train/architecture.py
+++ reconstruction/train/architecture.py
@@ -5,26 +5,25 @@ class RGBToHyperSpectralNet(nn.Module):
     def __init__(self, input_channels, output_channels):
         super().__init__()
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(input_channels, 64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=input_channels, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ELU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ELU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.ELU(),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.ELU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
         )
         self.upsample = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ELU(),
-            nn.ConvTranspose2d(64, output_channels, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(in_channels=64, out_channels=output_channels, kernel_size=3, stride=1, padding=1),
             nn.Sigmoid()
         )
 
     def forward(self, x):
         x = self.feature_extractor(x)
         x = self.upsample(x)
-        x = F.pad(x, (0, 0, 1, 1))
         return x
