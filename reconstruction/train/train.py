cbda314b1a025de645b5949a0836260591b2fa00
Christian Unigarro <christian.unigarro@zinobe.com>
2024-07-17 04:48:02 -0500

    Eval dataset_01 or dataset_02.

--- reconstruction/train/train.py
+++ reconstruction/train/train.py
@@ -7,17 +7,20 @@ import matplotlib.pyplot as plt
 from architecture import RGBToHyperSpectralNet
 from train_dataset import RGBToHyperSpectralDataset
 
+dataset_dir = './dataset_02'
+
 train_transforms = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Resize((924,691))
 ])
 
-rgb_dir = './dataset_02/train_rgb'
-hyperspectral_dir = './dataset_02/train_spectral'
+rgb_dir = f'{dataset_dir}/train_rgb'
+hyperspectral_dir = f'{dataset_dir}/train_spectral'
 dataset_train = RGBToHyperSpectralDataset(rgb_dir, hyperspectral_dir, transform=train_transforms)
 dataloader_train = DataLoader(dataset_train, shuffle=True, batch_size=16)
 
-val_rgb_dir = './dataset_02/valid_rgb'
-val_hyperspectral_dir = './dataset_02/valid_spectral'
+val_rgb_dir = f'{dataset_dir}/valid_rgb'
+val_hyperspectral_dir = f'{dataset_dir}/valid_spectral'
 dataset_val = RGBToHyperSpectralDataset(val_rgb_dir, val_hyperspectral_dir, transform=train_transforms)
 dataloader_val = DataLoader(dataset_val, batch_size=16)
 
@@ -41,16 +44,18 @@ for epoch in range(10):
         optimizer.step()
         train_losses.append(loss.item())
 
-    print(f'Epoch [{epoch+1}/10], Training Loss: {np.mean(train_losses):.4f}')
+    if 'dataset_02' in dataset_dir:
+        print(f'Epoch [{epoch+1}/10], Training Loss: {np.mean(train_losses):.4f}')
 
-    """ net.eval()
-    with no_grad():
-        for rgb_images_val, hyperspectral_images_val in dataloader_val:
-            outputs_val = net(rgb_images_val)
-            loss_val = criterion(outputs_val, hyperspectral_images_val)
-            val_losses.append(loss_val.item())
+    if 'dataset_01' in dataset_dir:
+        net.eval()
+        with no_grad():
+            for rgb_images_val, hyperspectral_images_val in dataloader_val:
+                outputs_val = net(rgb_images_val)
+                loss_val = criterion(outputs_val, hyperspectral_images_val)
+                val_losses.append(loss_val.item())
 
-    print(f'Epoch [{epoch+1}/10], Training Loss: {np.mean(train_losses):.4f}, Validation Loss: {np.mean(val_losses):.4f}') """
+        print(f'Epoch [{epoch+1}/10], Training Loss: {np.mean(train_losses):.4f}, Validation Loss: {np.mean(val_losses):.4f}')
 
 model_path = './model_weights.pth'
 save(net.state_dict(), model_path)
