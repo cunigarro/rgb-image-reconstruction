358e2c2a0ee39c3fb596ae7d01fb0752f825bb0c
Christian Unigarro <christian.unigarro@zinobe.com>
2024-08-25 14:16:22 -0500

    Add conditional GAN.

--- c_gan/train/discrimitator.py
+++ c_gan/train/discrimitator.py
@@ -2,23 +2,24 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-class Discriminator(nn.Module):
-    def __init__(self, num_classes, img_shape):
-        super(Discriminator, self).__init__()
-        self.label_emb = nn.Embedding(num_classes, num_classes)
+class NIRDiscriminator(nn.Module):
+    def __init__(self):
+        super(NIRDiscriminator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(num_classes + int(np.prod(img_shape)), 512),
+            nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 1),
-            nn.Sigmoid(),
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Flatten(),
+            nn.Linear(256 * 16 * 16, 1),
+            nn.Sigmoid()
         )
 
-    def forward(self, img, labels):
-        # Concatenar la imagen y las etiquetas
-        d_in = torch.cat((img.view(img.size(0), -1), self.label_emb(labels)), -1)
-        validity = self.model(d_in)
-        return validity
+    def forward(self, img):
+        return self.model(img)
+
--- c_gan/train/train.py
+++ c_gan/train/train.py
@@ -1,74 +1,55 @@
 import torch
+import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-import matplotlib.pyplot as plt
-from generator import Generator
-from discrimitator import Discriminator
+from generator import RGBToNIRGenerator
+from discrimitator import NIRDiscriminator
 
 # Hiperparámetros
-latent_dim = 100
-num_classes = 10
-img_shape = (1, 28, 28)
-batch_size = 64
 lr = 0.0002
 b1 = 0.5
 b2 = 0.999
-n_epochs = 200
-sample_interval = 400
+n_epochs = 100
+batch_size = 64
 
-# Preparar los datos de MNIST
-dataloader = DataLoader(
-    datasets.MNIST(
-        "data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose([
-            transforms.Resize(28),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5], [0.5])
-        ]),
-    ),
-    batch_size=batch_size,
-    shuffle=True,
-)
+# Preparar los datos: necesitas un dataset que tenga imágenes RGB y su correspondiente en 850nm
+# Aquí se deben agregar transformaciones y DataLoader para cargar los datos
 
 # Inicializar generador y discriminador
-generator = Generator(latent_dim, num_classes, img_shape)
-discriminator = Discriminator(num_classes, img_shape)
+generator = RGBToNIRGenerator()
+discriminator = NIRDiscriminator()
 
 # Optimizadores
 optimizer_G = optim.Adam(generator.parameters(), lr=lr, betas=(b1, b2))
 optimizer_D = optim.Adam(discriminator.parameters(), lr=lr, betas=(b1, b2))
 
-# Pérdida de entropía cruzada binaria
-adversarial_loss = torch.nn.BCELoss()
+# Pérdida de entropía cruzada binaria y de consistencia
+adversarial_loss = nn.BCELoss()
+consistency_loss = nn.L1Loss()
 
 for epoch in range(n_epochs):
-    for i, (imgs, labels) in enumerate(dataloader):
+    for i, (imgs_rgb, imgs_nir) in enumerate(dataloader):
 
-        batch_size = imgs.size(0)
+        batch_size = imgs_rgb.size(0)
 
         # Etiquetas reales y falsas
         valid = torch.ones(batch_size, 1, requires_grad=False)
         fake = torch.zeros(batch_size, 1, requires_grad=False)
 
         # Convertir imágenes y etiquetas a tensores
-        real_imgs = imgs
-        labels = labels
+        real_imgs = imgs_nir  # Imágenes en 850nm
+        rgb_imgs = imgs_rgb   # Imágenes RGB
 
         # Entrenamiento del Generador
         optimizer_G.zero_grad()
 
-        # Generar ruido y etiquetas aleatorias
-        z = torch.randn(batch_size, latent_dim)
-        gen_labels = torch.randint(0, num_classes, (batch_size,))
+        # Generar imágenes en 850nm a partir de imágenes RGB
+        gen_imgs = generator(rgb_imgs)
 
-        # Generar imágenes
-        gen_imgs = generator(z, gen_labels)
+        # Pérdida del generador (adversarial y de consistencia)
+        g_loss_adv = adversarial_loss(discriminator(gen_imgs), valid)
+        g_loss_consistency = consistency_loss(gen_imgs, real_imgs)
 
-        # Pérdida del generador
-        g_loss = adversarial_loss(discriminator(gen_imgs, gen_labels), valid)
+        g_loss = g_loss_adv + g_loss_consistency
 
         g_loss.backward()
         optimizer_G.step()
@@ -76,13 +57,10 @@ for epoch in range(n_epochs):
         # Entrenamiento del Discriminador
         optimizer_D.zero_grad()
 
-        # Pérdida para imágenes reales
-        real_loss = adversarial_loss(discriminator(real_imgs, labels), valid)
+        # Pérdida para imágenes reales y falsas
+        real_loss = adversarial_loss(discriminator(real_imgs), valid)
+        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
 
-        # Pérdida para imágenes generadas
-        fake_loss = adversarial_loss(discriminator(gen_imgs.detach(), gen_labels), fake)
-
-        # Pérdida total del discriminador
         d_loss = (real_loss + fake_loss) / 2
 
         d_loss.backward()
@@ -93,11 +71,3 @@ for epoch in range(n_epochs):
             f"[Epoch {epoch}/{n_epochs}] [Batch {i}/{len(dataloader)}] "
             f"[D loss: {d_loss.item()}] [G loss: {g_loss.item()}]"
         )
-
-        # Guardar muestras generadas
-        if i % sample_interval == 0:
-            gen_imgs = gen_imgs.view(gen_imgs.size(0), *img_shape)
-            plt.figure(figsize=(5, 5))
-            plt.imshow(gen_imgs[0].squeeze().detach().numpy(), cmap="gray")
-            plt.title(f"Generated Image with Label: {gen_labels[0]}")
-            plt.show()
