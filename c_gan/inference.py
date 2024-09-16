7258f4705bd3cdf6dd05e216d06fc94fcff85b51
Christian Unigarro <christian.unigarro@zinobe.com>
2024-09-15 20:15:51 -0500

    Add result file for inference.

--- c_gan/inference.py
+++ c_gan/inference.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 from train.generator import RGBToNIRGenerator
 
-# Ruta al archivo de pesos guardados
 generator_weights_path = './generator.pth'
 
 generator = RGBToNIRGenerator()
@@ -15,10 +14,10 @@ generator.eval()
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])  # Normalización
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
 
-image_path = './rgb_image.jpg'
+image_path = './rgb_image.jpeg'
 image_rgb = Image.open(image_path).convert('RGB')
 
 image_rgb_tensor = transform(image_rgb).unsqueeze(0)
@@ -26,7 +25,7 @@ image_rgb_tensor = transform(image_rgb).unsqueeze(0)
 with torch.no_grad():
     nir_generated_tensor = generator(image_rgb_tensor)
 
-nir_generated_tensor = nir_generated_tensor.squeeze(0) * 0.5 + 0.5  # Convertir el tensor a rango [0,1]
+nir_generated_tensor = nir_generated_tensor.squeeze(0) * 0.5 + 0.5
 
 nir_generated_image = transforms.ToPILImage()(nir_generated_tensor)
 
