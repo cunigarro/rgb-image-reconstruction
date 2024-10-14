e1873e828a1cbc68ef828b87dc89de1bad580bd8
Christian Unigarro <christian.unigarro@zinobe.com>
2024-10-13 23:16:54 -0500

    Improve requirements and show rgb image aligned.

--- c_gan/pre_process/registration.py
+++ c_gan/pre_process/registration.py
@@ -68,37 +68,36 @@ class ProyectividadOpenCV():
         return None
 
 def main():
-    print("Comenzado proceso con imágenes de prueba propias \n")
-    print("Por favor ingrese ancho y alto deseado para las imágenes \npor ejemplo 700,500. De no asignarlos, los valores por defecto serán 700X500 \n \n")
-    print("Ancho=")
-    width_str = input()
-    print("Alto=")
-    height_str = input()
-
-    width = int(width_str)
-    height = int(height_str)
+    width = 700
+    height = 500
 
     example_2 = ProyectividadOpenCV()
 
-    img_RGB = Image.open('c_gan/pre_process/sequoia_images/img_RGB.JPG')
-    img_RGB = ImageOps.exif_transpose(img_RGB)
-    img_RGB = np.asarray(img_RGB)
+    img_RGB_PIL = Image.open('c_gan/pre_process/sequoia_images/img_RGB.JPG')
+    img_RGB_PIL = ImageOps.exif_transpose(img_RGB_PIL)
+    img_RGB_PIL = np.asarray(img_RGB_PIL)
+
+    img_RED = img_RGB_PIL[:,:,0]
+
+    img_RGB = cv2.imread("c_gan/pre_process/sequoia_images/img_RGB.JPG", 0)
     img_NIR = cv2.imread("c_gan/pre_process/sequoia_images/img_NIR.TIF", 0)
 
-    img_RED = img_RGB[:,:,0]
     img_NULL = np.zeros(width*height, dtype=np.uint8).reshape(width,height)
 
+    img_RGB = cv2.resize(img_RGB, (width, height), interpolation=cv2.INTER_LINEAR)
     img_RED = cv2.resize(img_RED, (width, height), interpolation=cv2.INTER_LINEAR)
+    img_NIR = cv2.resize(img_NIR, (width, height), interpolation=cv2.INTER_LINEAR)
     img_NULL = cv2.resize(img_NULL, (width, height), interpolation=cv2.INTER_LINEAR)
 
-    merged_fix_bad = cv2.merge((img_RED, img_NIR,img_NULL))
+    merged_fix_bad = cv2.merge((img_RGB, img_NIR, img_NULL))
 
     stb_RGB, stb_NIR, stb_RED = example_2.img_alignment_sequoia(img_RGB, img_NIR, img_RED)
 
     mask_NIR = (stb_NIR > 0).astype(np.uint8)
     mask_RED = (stb_RED > 0).astype(np.uint8)
+    mask_RGB = (stb_RGB > 0).astype(np.uint8)
 
-    mask_intersection = cv2.bitwise_and(mask_RED, mask_NIR)
+    mask_intersection = cv2.bitwise_and(mask_RED, mask_NIR, mask_RGB)
 
     contours, _ = cv2.findContours(mask_intersection, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -106,11 +105,13 @@ def main():
 
     cropped_RED = stb_RED[y:y+h, x:x+w]
     cropped_NIR = stb_NIR[y:y+h, x:x+w]
+    cropped_RGB = stb_RGB[y:y+h, x:x+w]
 
     cv2.imwrite('cropped_RED.jpg', cropped_RED)
     cv2.imwrite('cropped_NIR.jpg', cropped_NIR)
+    cv2.imwrite('cropped_RGB.jpg', cropped_RGB)
 
-    merged_fix_stb = cv2.merge((stb_RED, stb_NIR, img_NULL))
+    merged_fix_stb = cv2.merge((stb_RGB, stb_NIR, img_NULL))
 
     print("La primera imagen que se genera simplemente superpone las imágenes sin alinear \n Cerrar la ventana para continuar \n")
     cv2.imshow('frame', merged_fix_bad)
