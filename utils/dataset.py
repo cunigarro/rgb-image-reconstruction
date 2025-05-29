66fc431d96ea441a1085b6cddaac1c4797b35ed1
Christian Unigarro <cunigarrod@gmail.com>
2025-05-28 21:41:35 -0500

    sr awan improved.

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,11 +5,12 @@ from torch.utils.data import Dataset
 import numpy as np
 
 class SequoiaDatasetNIR_S3(Dataset):
-    def __init__(self, bucket_name, rgb_keys, nir_keys, transform=None):
+    def __init__(self, bucket_name, rgb_keys, nir_keys, img_size=None, transform=None):
         self.s3 = boto3.client('s3')
         self.bucket = bucket_name
         self.rgb_keys = rgb_keys
         self.nir_keys = nir_keys
+        self.img_size = img_size
         self.transform = transform
 
         assert len(self.rgb_keys) == len(self.nir_keys), "Mismatch in number of RGB and NIR images"
@@ -29,12 +30,12 @@ class SequoiaDatasetNIR_S3(Dataset):
         # RGB image from S3
         rgb_image = self.read_image_from_s3(self.rgb_keys[idx], grayscale=False)
         rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB)
-        rgb_image = rgb_image / 255.0  # no resize
+        rgb_image = cv2.resize(rgb_image, self.img_size) / 255.0
         rgb_image = np.transpose(rgb_image, (2, 0, 1)).astype(np.float32)
 
         # NIR image from S3 (grayscale)
         nir_image = self.read_image_from_s3(self.nir_keys[idx], grayscale=True)
-        nir_image = nir_image / 255.0  # no resize
+        nir_image = cv2.resize(nir_image, self.img_size) / 255.0
         nir_image = np.expand_dims(nir_image, axis=0).astype(np.float32)
 
         return torch.tensor(rgb_image), torch.tensor(nir_image)
