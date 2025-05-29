8e13eec386d1ee5e37dc2ed9ed77f51df7a6239f
Christian Unigarro <cunigarrod@gmail.com>
2025-05-28 20:24:42 -0500

    SR UNET improved.

--- sr_unet/train.py
+++ sr_unet/train.py
@@ -19,7 +19,7 @@ bucket_name = 'dataset-rgb-nir-01'
 # Dataset completo (300 imágenes)
 rgb_keys = list_s3_files(bucket_name, 'rgb_images/')
 nir_keys = list_s3_files(bucket_name, 'nir_images/')
-full_dataset = SequoiaDatasetNIR_S3(bucket_name, rgb_keys, nir_keys, img_size=(256, 256))
+full_dataset = SequoiaDatasetNIR_S3(bucket_name, rgb_keys, nir_keys)
 print(f"Total imágenes en dataset: {len(full_dataset)}")
 
 # Dividir en train y val
@@ -34,7 +34,7 @@ val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 # Dataset de prueba (60 imágenes)
 test_rgb_keys = list_s3_files(bucket_name, 'rgb_images_test/')
 test_nir_keys = list_s3_files(bucket_name, 'nir_images_test/')
-test_dataset = SequoiaDatasetNIR_S3(bucket_name, test_rgb_keys, test_nir_keys, img_size=(256, 256))
+test_dataset = SequoiaDatasetNIR_S3(bucket_name, test_rgb_keys, test_nir_keys)
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
 # Modelo y demás componentes (igual)
